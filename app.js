681b3ce9fbacf116614bcbcb4976d3c613e456f9
to0 <yuanancai@gmail.com>
2014-05-08 23:07:27 +0800

    ca

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('env','development');
 
 app.use(favicon());
 app.use(logger('dev'));
---
+++ public/js/home.js
@@ -0,0 +1,37 @@
+$(document).ready(function($){
+	// if( (navigator.userAgent.match(/Android/i)|| navigator.userAgent.match(/webOS/i)|| navigator.userAgent.match(/iPhone/i)|| navigator.userAgent.match(/iPad/i)|| navigator.userAgent.match(/iPod/i)|| navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) )){
+	if($(window).width() <= 768){
+		$( ".spacer" ).remove();
+	}
+	else{
+		$(window).stellar();
+		$('.scrollTo').click(function(e) {
+			e.preventDefault();
+			var bandId = $(this).attr('href');
+			console.log(bandId);
+
+			$('html, body').animate({
+					scrollTop: $(bandId).offset().top}, 'slow'
+			);
+
+		});
+	}
+
+	$(".spacer").css("height",$(window).height()+'px');
+	$(".intro").attr("height",$(window).height()+'px');
+	$( window ).resize(function() {
+		if($(window).width() <= 768){
+			$( ".spacer" ).remove();
+		}
+		else {
+			if($('.spacer').length==0){
+				$('body').prepend('<div class="spacer"></div>');
+			}
+		}
+		$(".spacer").css("height",$(window).height()+'px');
+		$(".intro").attr("height",$(window).height()+'px');
+	});
+
+
+
+});

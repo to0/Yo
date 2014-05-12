94381aaccd7854187d7315d0de92586e157cc1cf
to0 <yuanancai@gmail.com>
2014-05-12 18:04:45 +0800

    home

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,5 @@
 $(document).ready(function($){
-	if( (navigator.userAgent.match(/Android/i)|| navigator.userAgent.match(/webOS/i)|| navigator.userAgent.match(/iPhone/i)|| navigator.userAgent.match(/iPad/i)|| navigator.userAgent.match(/iPod/i)|| navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) )){
+	if( (navigator.userAgent.match(/Android/i)|| navigator.userAgent.match(/webOS/i)|| navigator.userAgent.match(/iPhone/i)|| navigator.userAgent.match(/iPad/i)|| navigator.userAgent.match(/iPod/i)|| navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) )||$(window).width() <= 768){
 	// if($(window).width() <= 768){
 		$( ".spacer" ).remove();
 	}

c1c57beb80bee2104c5b43f86d3bbc6aeb1a8654
to0 <yuanancai@gmail.com>
2014-05-12 18:00:54 +0800

    home

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function($){
-	// if( (navigator.userAgent.match(/Android/i)|| navigator.userAgent.match(/webOS/i)|| navigator.userAgent.match(/iPhone/i)|| navigator.userAgent.match(/iPad/i)|| navigator.userAgent.match(/iPod/i)|| navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) )){
-	if($(window).width() <= 768){
+	if( (navigator.userAgent.match(/Android/i)|| navigator.userAgent.match(/webOS/i)|| navigator.userAgent.match(/iPhone/i)|| navigator.userAgent.match(/iPad/i)|| navigator.userAgent.match(/iPod/i)|| navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) )){
+	// if($(window).width() <= 768){
 		$( ".spacer" ).remove();
 	}
 	else{

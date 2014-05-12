658ba80965d298e1c65ffe5c2049b529aaf24fd0
to0 <yuanancai@gmail.com>
2014-05-12 23:11:58 +0800

    download link

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index');
 });
-
+router.get('/download-android', function(req, res) {
+	res.download(path.resolve(__dirname,'../tmp/yohelper.apk'));
+});
 module.exports = router;
